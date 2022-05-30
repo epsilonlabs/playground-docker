88bf44310d85387ef6f131fc02f12910212f8bd4
Antonio García-Domínguez <a.garcia-dominguez@aston.ac.uk>
2022-05-30 17:42:16 +0100

    Revert changes after tweaking CPU settings in Cloud Run

--- start.sh
+++ start.sh
@@ -2,18 +2,14 @@
 
 set -e
 
-export MAVEN_OPTS="-Xmx2g"
-
 # Functions for running Epsilon
-mvn -B -o function:run -Drun.functionTarget=org.eclipse.epsilon.live.RunEpsilonFunction -Drun.port=8001 &> mvn8001.log &
-mvn -B -o function:run -Drun.functionTarget=org.eclipse.epsilon.live.FlexmiToGraphvizFunction -Drun.port=8002 &> mvn8002.log &
-mvn -B -o function:run -Drun.functionTarget=org.eclipse.epsilon.live.EmfaticToGraphvizFunction -Drun.port=8003 &> mvn8003.log &
+mvn -B -o function:run -Drun.functionTarget=org.eclipse.epsilon.live.RunEpsilonFunction -Drun.port=8001 &
+mvn -B -o function:run -Drun.functionTarget=org.eclipse.epsilon.live.FlexmiToGraphvizFunction -Drun.port=8002 &
+mvn -B -o function:run -Drun.functionTarget=org.eclipse.epsilon.live.EmfaticToGraphvizFunction -Drun.port=8003 &
 
 # nginx as frontend + reverse proxy
 envsubst < /etc/nginx.conf.template > /etc/nginx/conf.d/default.conf
 nginx -g "daemon off;" &
-
-tail -f mvn*.log &
 
 # wait for them all
 wait -n
