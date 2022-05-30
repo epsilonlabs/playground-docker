5e58370722c8c5683f1b5c8db7db47aa8c2d49c4
Antonio García-Domínguez <a.garcia-dominguez@aston.ac.uk>
2022-05-30 15:33:06 +0100

    Try using IPv4 in Cloud Run

--- start.sh
+++ start.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Force use of IPv4 (for Google Cloud Run)
+export MAVEN_OPTS="-Djava.net.preferIPv4Stack=true"
+
 mvn -B -o function:run -Drun.functionTarget=org.eclipse.epsilon.live.RunEpsilonFunction -Drun.port=8001 &
 mvn -B -o function:run -Drun.functionTarget=org.eclipse.epsilon.live.FlexmiToGraphvizFunction -Drun.port=8002 &
 mvn -B -o function:run -Drun.functionTarget=org.eclipse.epsilon.live.EmfaticToGraphvizFunction -Drun.port=8003 &
